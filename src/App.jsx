c7fb2638c858577ab80ac7ee2e89ffb19ccf1df6
amir-dev5 <amirrepo595@gmail.com>
2024-07-11 09:26:19 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <h1 className="recipe-title">Recipe Manager</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
